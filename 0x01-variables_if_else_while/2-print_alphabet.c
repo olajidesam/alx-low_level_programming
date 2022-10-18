336b4cbfe3c6e56164aa1fcacbf3fcb578414588
Dada Olajide <olajidedada74@gmail.com>
2022-10-17 20:23:25 -0700

    Created 2-print_alphabet.c task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - This prints alphabets in lower case
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
+
