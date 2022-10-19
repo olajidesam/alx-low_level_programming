1c46eedd7d82fad7fa3cd37ae61345b2aa963903
Dada Olajide <olajidedada74@gmail.com>
2022-10-19 11:56:01 -0700

    Created 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * @k: first number
+ * @m: second number
+ * Return: addition of both numbers
+ */
+int add(int k, int m)
+{
+	return (k + m);
+}
