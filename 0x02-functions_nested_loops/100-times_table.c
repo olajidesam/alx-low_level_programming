a5ed7828d5027ae0021a8f757771a728242fa08b
Dada Olajide <olajidedada74@gmail.com>
2022-10-19 22:16:45 -0700

    Advanced times-table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints times table
+ *
+ * @n: integer number
+ */
+void print_times_table(int n)
+{
+	int i;
+	int j;
+
+	int n = 9;
+
+	if (n > 15 || n < 0)
+		printf("Nothing")
+
+			for (i = 0; i <= n; i++)
+				for (j = 0; j <= n; j++)
+				{
+					printf("Table");
+				}
+}
